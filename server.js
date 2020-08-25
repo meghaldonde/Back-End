ed4af55d966acf216a8f38470d5515a244816e5c
karimkhattaby <18755153+karimkhattaby@users.noreply.github.com>
2020-08-25 13:21:32 +0400

    Refactored database connection code & Added Comments

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+//Import Dependencies
+const mongoose = require('mongoose'); // Used for MongoDB
+require('dotenv').config(); // Used for environmental variables
 
+//Import Application Middlewares & Routes
+const app = require('./app');
+
+//Handle Exceptions
 process.on('uncaughtException', (err) => {
   console.log('UNCAUGHT EXCEPTION! 🤯 Shutting down....');
   console.log(err.name, err.message);
-
   process.exit(1);
 });
 
-const app = require('./app');
+//Handle Rejections
+process.on('unhandledRejection', (err) => {
+  console.log('UNHADLED REJECTION! 🤯 Shutting down....');
+  console.log(err.name, err.message);
+  //Here we give the server time to finish all the requests that are still pending
+  //or being handled at the time, and only after that the server is killed
+  server.close(() => {
+    process.exit(1);
+  });
+});
 
-//Use this if you will use a local database
-// mongoose.connect('mongodb://localhost:27017/GrowersBrains')
-// .then(() => console.log('Successfully connected to the database!'));
-
+//Connect to database
+//TODO: Change MONGO_URI env variable in .env file if you're using a hosted database
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -22,19 +33,12 @@ mongoose
     useCreateIndex: true,
   })
   .then(() => console.log('Successfully connected to the database!'));
-//No need to add the catch block because we add an event listener to the `unhandledRejection` he will take care off it
+//No need to add the catch block
+//`uncaughtException` and `unhandledRejection` which will take care of that
 
+//Start listening to requests
+//TODO: Change PORT env variable in .env file. Default port is 5000
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}....`);
-});
-
-process.on('unhandledRejection', (err) => {
-  console.log('UNHADLED REJECTION! 🤯 Shutting down....');
-  console.log(err.name, err.message);
-  //Here we give the server time to finish all the requests that are still pending
-  //or being handled at the time, and only after that the server is thein basically killed
-  server.close(() => {
-    process.exit(1);
-  });
 });
